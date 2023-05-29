10564d6438dbbca049b807cd1223578acd258943
Amanda <107001164+amandadeusilva@users.noreply.github.com>
2023-05-29 09:06:02 -0300

    commit inicial

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// botões Git e linkedin
+let bt_github = document.querySelector('.bt_github');
+bt_github.addEventListener('click',()=>{
+    window.location.href = "https://github.com/amandadeusilva";
+})
+let bt_linkedin = document.querySelector('.bt_linkedin');
+bt_linkedin.addEventListener('click',()=>{
+    window.location.href = "https://linkedin.com/in/amanda-silva-a4729366";
+})
