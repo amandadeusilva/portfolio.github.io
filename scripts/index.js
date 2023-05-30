6085f00c184e2ef301ed06958c783eb8e65c43c6
Amanda <107001164+amandadeusilva@users.noreply.github.com>
2023-05-30 09:38:52 -0300

    adicionando responsividade

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,12 @@ let bt_linkedin = document.querySelector('.bt_linkedin');
 bt_linkedin.addEventListener('click',()=>{
     window.location.href = "https://linkedin.com/in/amanda-silva-a4729366";
 })
+window.onload = function(){
+  document.querySelector(".navbar__mobile").addEventListener("click", function(){
+  if(document.querySelector(".navbar__mobile ul").style.display =='flex') {
+      document.querySelector(".navbar__mobile ul").style.display ='none';
+  } else{
+      document.querySelector(".navbar__mobile ul").style.display ='flex';
+  }
+  });
+};
